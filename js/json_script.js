258e41f6e0e1ea9a824a182cc833d559c0fa4bc2
Laioxy <yamaneko062@gmail.com>
2024-01-10 22:10:37 +0900

    初回コミット

---
+++ js/json_script.js
@@ -0,0 +1,40 @@
+// ポケモンデータをJSONから取得
+async function GetPokemonJson() {
+  let res;
+  await $.getJSON("./data/pokemon_data.json", function (json) {
+    res = JSON.parse(JSON.stringify(json));
+  });
+  return res;
+}
+// 道具データをJSONから取得
+async function GetItemJson() {
+  let res;
+  await $.getJSON("./data/item_data.json", function (json) {
+    res = JSON.parse(JSON.stringify(json));
+  });
+  return res;
+}
+// ダンジョンデータをJSONから取得
+async function GetDungeonJson() {
+  let res;
+  await $.getJSON("./data/dungeon_data.json", function (json) {
+    res = JSON.parse(JSON.stringify(json));
+  });
+  return res;
+}
+// フロアデータをJSONから取得
+async function GetFloorJson() {
+  let res;
+  await $.getJSON("./data/floor_data.json", function (json) {
+    res = JSON.parse(JSON.stringify(json));
+  });
+  return res;
+}
+// 固定フロアデータをJSONから取得
+async function GetFixedJson() {
+  let res;
+  await $.getJSON("./data/fixed_data.json", function (json) {
+    res = JSON.parse(JSON.stringify(json));
+  });
+  return res;
+}
---
+++ js/poke_prop.js
@@ -0,0 +1,29 @@
+// タイプ
+var poke_type = [
+  { id: 0, name: "なし" },
+  { id: 1, name: "ノーマル" },
+  { id: 2, name: "ほのお" },
+  { id: 3, name: "みず" },
+  { id: 4, name: "くさ" },
+  { id: 5, name: "でんき" },
+  { id: 6, name: "こおり" },
+  { id: 7, name: "かくとう" },
+  { id: 8, name: "どく" },
+  { id: 9, name: "じめん" },
+  { id: 10, name: "ひこう" },
+  { id: 11, name: "エスパー" },
+  { id: 12, name: "むし" },
+  { id: 13, name: "いわ" },
+  { id: 14, name: "ゴースト" },
+  { id: 15, name: "ドラゴン" },
+  { id: 16, name: "あく" },
+  { id: 17, name: "はがね" },
+];
+
+// 性別
+var poke_gender = [
+  { id: 0, name: "無効" },
+  { id: 1, name: "オス" },
+  { id: 2, name: "メス" },
+  { id: 3, name: "不明" },
+];
