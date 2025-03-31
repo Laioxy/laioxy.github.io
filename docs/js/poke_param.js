79aa21b56b8ab9e26a385d3cc8157b2865d616bd
laioxy <yamaneko062@gmail.com>
2025-03-31 20:59:52 +0900

    表示用ページ更新 2025-03-31 (2)

--- docs/js/poke_param.js
+++ docs/js/poke_param.js
@@ -18,24 +18,6 @@ const poke_type = [
   { id: 15, name: 'ドラゴン' },
   { id: 16, name: 'あく' },
   { id: 17, name: 'はがね' },
-  { id: 0, name: 'なし' },
-  { id: 1, name: 'ノーマル' },
-  { id: 2, name: 'ほのお' },
-  { id: 3, name: 'みず' },
-  { id: 4, name: 'くさ' },
-  { id: 5, name: 'でんき' },
-  { id: 6, name: 'こおり' },
-  { id: 7, name: 'かくとう' },
-  { id: 8, name: 'どく' },
-  { id: 9, name: 'じめん' },
-  { id: 10, name: 'ひこう' },
-  { id: 11, name: 'エスパー' },
-  { id: 12, name: 'むし' },
-  { id: 13, name: 'いわ' },
-  { id: 14, name: 'ゴースト' },
-  { id: 15, name: 'ドラゴン' },
-  { id: 16, name: 'あく' },
-  { id: 17, name: 'はがね' },
 ];
 
 // 性別
@@ -44,20 +26,10 @@ const poke_gender = [
   { id: 1, name: 'オス' },
   { id: 2, name: 'メス' },
   { id: 3, name: '不明' },
-  { id: 0, name: '無効' },
-  { id: 1, name: 'オス' },
-  { id: 2, name: 'メス' },
-  { id: 3, name: '不明' },
 ];
 
 // 移動タイプ
 const poke_movetype = [
-  { id: 0, name: '通常' },
-  { id: 1, name: '? (不明)' },
-  { id: 2, name: '空中' },
-  { id: 3, name: '通過' },
-  { id: 4, name: 'マグマ' },
-  { id: 5, name: '水上' },
   { id: 0, name: '通常' },
   { id: 1, name: '? (不明)' },
   { id: 2, name: '空中' },
